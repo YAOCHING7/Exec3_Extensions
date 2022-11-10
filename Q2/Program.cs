271b93c552d815c3c561d139e13bacc2826701ce
ispan <ispan@DESKTOP-PAKSETB>
2022-11-10 17:05:16 +0800

    加入專案檔案。

---
+++ Q2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string value = "0123456789";
+
+			string result = value.Left(5);
+			Console.WriteLine(result);
+
+		}
+	}
+	public static class StringLeft
+	{
+		public static string Left(this string source, int length)
+		{
+			if (string.IsNullOrEmpty(source)) return string.Empty;
+			if (length <= 0) return string.Empty;
+			if (source.Length <= length) return source;
+
+			return source.Substring(0, length);
+		}
+	}
+}
---
+++ Q3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string value = "123";
+			int number = value.ToInt(-1); // 若轉型失敗, 傳回-1
+			Console.WriteLine(number);
+		}
+	}
+	public static class StringExtensions
+	{
+		public static int ToInt(this string source, int defaultValue)
+		{
+			bool isInt = int.TryParse(source, out int number);
+
+			return isInt ? number : defaultValue;
+		}
+	}
+}
